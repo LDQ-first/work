593b49a13cab7ca22e032fdce22260499083dadc
LDQ-first <178848890@qq.com>
2017-09-25 10:41:33 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 
 $.fn.extend({
     Waterfall: function() {
+        $('.loading').removeClass('none');
         var $this = this;
         var father = $this.parent()
+
+         var defereds = [];
+            $this.find('img').each(function() {
+                var defer = $.Deferred();
+                $(this).on('load', function() {
+                    defer.resolve();
+                });
+                defereds.push(defer);
+            })
+            $.when.apply(null, defereds)
+                  .done(function() {
+                      render()
+                  })
 
         function render(){
             var nodeWidth = $this.outerWidth(true),
@@ -13,30 +27,37 @@ $.fn.extend({
                 colSumHeight.push(0);
             }
 
-            $this.each(function(){
-                var $cur = $(this); 
-                var idx = 0,
-                    minSumHeight = colSumHeight[0];
+           
 
-                for(var i = 0; i < colSumHeight.length; i++){
-                    if(colSumHeight[i] < minSumHeight){
-                        idx = i;
-                        minSumHeight = colSumHeight[i];
+
+                 $this.each(function(){
+                    var $cur = $(this); 
+                    var idx = 0,
+                        minSumHeight = colSumHeight[0];
+
+                    for(var i = 0; i < colSumHeight.length; i++){
+                        if(colSumHeight[i] < minSumHeight){
+                            idx = i;
+                            minSumHeight = colSumHeight[i];
+                        }
                     }
-                }
 
-                $cur.css({
-                    left: nodeWidth * idx,
-                    top: minSumHeight
+                    $cur.css({
+                        left: nodeWidth * idx,
+                        top: minSumHeight
+                    })
+
+                    colSumHeight[idx] += $cur.outerHeight(true);
+                }); 
+            
+            
+                var maxHeight = Math.max.apply(null, colSumHeight);
+                father.css({
+                    height: maxHeight,
+                    width: nodeWidth * colNum
                 })
-
-                colSumHeight[idx] += $cur.outerHeight(true);
-            }); 
-            var maxHeight = Math.max.apply(null, colSumHeight);
-            father.css({
-                height: maxHeight,
-                width: nodeWidth * colNum
-            })
+                
+                $('.loading').addClass('none');
 
         }
         render();
